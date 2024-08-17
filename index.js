8335b8029124be7db8ede6691e44948dbdc9fcfa
huuhuy2910 <131759541+huuhuy2910@users.noreply.github.com>
2024-08-17 23:03:35 +0700

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+})(jQuery);
